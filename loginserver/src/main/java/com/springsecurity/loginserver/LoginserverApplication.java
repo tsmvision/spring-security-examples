995e1a6c52276fc8a3797e9feb9285c075fab570
lukelee <lukelee01@gmail.com>
2021-08-13 19:16:57 -0400

    create loginserver

---
+++ loginserver/src/main/java/com/springsecurity/loginserver/LoginserverApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.loginserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginserverApplication.class, args);
+    }
+
+}
---
+++ loginserver/src/test/java/com/springsecurity/loginserver/LoginserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.loginserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
